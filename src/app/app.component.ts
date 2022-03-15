69285d1c5dd20130b99f7d21f5fea5401078c834
arminkoliyaie <akoliaye@gmail.com>
2022-03-15 09:51:06 +0330

    commuting form PC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular';
+  password: string = '';
+  includeNumber: boolean = false;
+  inclideSymbols: boolean = false;
+  inclideLatter: boolean = false;
+  Lenght: number = 0;
+
+  onButtonClick() {
+    const number = '123456789';
+    const symbols = 'abcdefghijklmpqrstuvwxyz';
+    const latters = '!@#$%&*';
+
+    let ValidChar = '';
+    if (this.includeNumber) {
+      ValidChar += number;
+    }
+    if (this.inclideSymbols) {
+      ValidChar += symbols;
+    }
+    if (this.inclideLatter) {
+      ValidChar += latters;
+    }
+
+    let generatePassword = '';
+    for (let i = 0; i < this.Lenght; i++) {
+      let i = Math.floor(Math.random() * ValidChar.length);
+      generatePassword += ValidChar[i];
+    }
+    this.password = generatePassword;
+  }
+
+  onChangeLatter() {
+    this.inclideLatter = !this.inclideLatter;
+  }
+  onchangeNumber() {
+    this.includeNumber = !this.includeNumber;
+  }
+  onchangeSymbols() {
+    this.inclideSymbols = !this.inclideSymbols;
+  }
+
+  onChangeLenght(value: string) {
+    const parseValid = parseInt(value);
+    if (!isNaN(parseValid)) {
+      this.Lenght = parseValid;
+    }
+  }
 }
